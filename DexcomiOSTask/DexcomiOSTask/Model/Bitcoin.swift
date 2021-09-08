60d9eef14fc5bcf73dba47a34ecbcace790e204b
Bharat Kumar <Bharkumar8@gmail.com>
2021-09-08 13:35:24 -0700

    Added bitcoin model

---
+++ DexcomiOSTask/DexcomiOSTask/Model/Bitcoin.swift
@@ -0,0 +1,15 @@
+//
+//  Bitcoin.swift
+//  DexcomiOSTask
+//
+//  Created by Bharat Kumar on 9/8/21.
+//
+
+import Foundation
+
+struct Bitcoin: Decodable {
+    var currency: String
+    var rate: Double
+    var description: String
+
+}
